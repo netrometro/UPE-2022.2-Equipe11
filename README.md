6268afc9950673d2d3ff2f1ba5700f8debdd8b17
renan-guilherme <87336450+renan-guilherme@users.noreply.github.com>
2023-03-16 11:22:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# UPE-2022.2-Equipe11
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-2fiady)
---
+++ passport-config.js
@@ -0,0 +1,29 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+
+async function initialize(passport, getUserByEmail, getUserById) {
+  const authenticateUser = async (email, password, done) => {
+    const user = getUserByEmail(email);
+    if (user == null) {
+      return done(null, false, { message: 'Email não registrado' });
+    }
+
+    try {
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: 'Senha errada' });
+      }
+    } catch (e) {
+      return done(e);
+    }
+  };
+
+  passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser));
+  passport.serializeUser((user, done) => done(null, user.id));
+  passport.deserializeUser((id, done) => {
+    return done(null, getUserById(id));
+  });
+}
+
+module.exports = initialize;
